8600846232bb659a7fe1a5882f9e86d4bd2a19fa
alitvin <litwinoff@gmail.com>
2017-02-20 07:58:30 +0300

    JPA with java-class config style: DataSource from .properties

--- JpaPostgresMaven/src/main/java/com/ayulit/config/DataSourcesConfig.java
+++ JpaPostgresMaven/src/main/java/com/ayulit/config/DataSourcesConfig.java
@@ -2,23 +2,30 @@ package com.ayulit.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
+@PropertySource("jdbc.properties")
 public class DataSourcesConfig {
 
+	@Autowired
+	private Environment environment;
+	
 	/* dataSource is like generalised factory for connections
 	 * We will use Spring's realization of 'dataSource'*/
 	@Bean
 	public DataSource getDataSource() {
 		DriverManagerDataSource ds = new DriverManagerDataSource();
-		ds.setDriverClassName("org.postgresql.Driver");
-		ds.setUrl("jdbc:postgresql://localhost:5432/grocery");
-        ds.setUsername("postgres");
-        ds.setPassword("123");
-		
+		ds.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+        ds.setUrl(environment.getRequiredProperty("jdbc.url"));
+        ds.setUsername(environment.getRequiredProperty("jdbc.username"));
+        ds.setPassword(environment.getRequiredProperty("jdbc.password"));
+        		
         return ds;		
 	}
 
